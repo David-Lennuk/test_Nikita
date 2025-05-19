d89f6cb0f44068706bced331dc863db3afe87ad8
David <153904926+David-Lennuk@users.noreply.github.com>
2025-05-19 13:28:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Endpoint /travel
 app.get('/travel', (req, res) => {
-  // Muuda "Jaapan" oma tegelikuks lemmikuks sihtkohaks!
   res.send('Minu lemmik reisisihtkoht on Jaapan.');
 });
 
