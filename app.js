5f44dae2ad15b6d14f568fcb1cd615f3efc0aff2
David <153904926+David-Lennuk@users.noreply.github.com>
2025-05-19 13:47:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/travel', (req, res) => {
   res.send('Minu lemmik reisisihtkoht on Jaapan.');
 });
 
-app.listen(port, () => {
-  console.log(`Rakendus kuulab pordil ${port}`);
+app.listen(3000, ()=> {
+  console.log("started");
 });
